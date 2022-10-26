872978797d8a8a2bb533eb9cb2621fd981c2f5ba
drakoo369 <drakoo@thetribecrypto.com>
2022-10-26 11:39:48 -0500

    fetching Transfer events v0.0.1

--- src/mur-all-nft.ts
+++ src/mur-all-nft.ts
@@ -1,113 +1,27 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  MurAllNFT,
-  Approval,
-  ApprovalForAll,
-  ArtworkFilled,
-  NewMetadataEncoderSet,
-  OwnershipTransferred,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer,
-  TransferWithMetadata
+  Transfer as TransferEvent,
+  MurAllNFT as MurAllNFTContract,
 } from "../generated/MurAllNFT/MurAllNFT"
-import { ExampleEntity } from "../generated/schema"
+import {Token, User } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if(!token){
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.createdAtTimestamp = event.block.timestamp;
+    let tokenContract = MurAllNFTContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+  }
+  token.owner = event.params.to.toHexString();
+  token.save();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let user = User.load(event.params.to.toHexString());
+  if(!user){
+    user = new User(event.params.to.toHexString());
+    user.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ADMIN_ROLE(...)
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.balanceOf(...)
-  // - contract.baseURI(...)
-  // - contract.childChainManagerProxy(...)
-  // - contract.encodeTokenMetadata(...)
-  // - contract.getAlphaChannel(...)
-  // - contract.getApproved(...)
-  // - contract.getArtist(...)
-  // - contract.getArtworkDataHashForId(...)
-  // - contract.getArtworkFillCompletionStatus(...)
-  // - contract.getArtworkForId(...)
-  // - contract.getFullDataForId(...)
-  // - contract.getName(...)
-  // - contract.getNumber(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getRoleMember(...)
-  // - contract.getRoleMemberCount(...)
-  // - contract.getSeriesId(...)
-  // - contract.getTotalArtworks(...)
-  // - contract.hasAlphaChannel(...)
-  // - contract.hasRole(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isArtworkFilled(...)
-  // - contract.mediaURI(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-  // - contract.viewURI(...)
-  // - contract.withdrawnTokens(...)
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleArtworkFilled(event: ArtworkFilled): void {}
-
-export function handleNewMetadataEncoderSet(
-  event: NewMetadataEncoderSet
-): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
-
-export function handleRoleGranted(event: RoleGranted): void {}
-
-export function handleRoleRevoked(event: RoleRevoked): void {}
-
-export function handleTransfer(event: Transfer): void {}
-
-export function handleTransferWithMetadata(event: TransferWithMetadata): void {}
+ 
